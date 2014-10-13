16309b787b956922919c8d7904dd68e9ed641601
Matthew Sykes <matthew.sykes@gmail.com>
2014-10-13 08:11:23 -0400

    Set vm and disk names, add vagrant to admin group

--- packer/scripts/ruby.sh
+++ packer/scripts/ruby.sh
@@ -9,14 +9,19 @@ if [ -d "${RBENV_ROOT}" ]; then
 fi
 EOF
 
+sudo -n apt-get update
+sudo -n apt-get install -y libssl-dev libreadline-dev libxml2 libxml2-dev libxslt1-dev
+sudo -n apt-get clean
+
 curl -s https://raw.githubusercontent.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
 export PATH="${HOME}/.rbenv/bin:${PATH}"
 hash -r
 eval "$(rbenv init -)"
 
-rbenv bootstrap-ubuntu-12-04
 rbenv install 1.9.3-p547
 rbenv global 1.9.3-p547
+
+echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 
 # Required for pre-packaging
 gem install bundler --no-ri --no-rdoc
--- packer/scripts/vagrant-sudoers.sh
+++ packer/scripts/vagrant-sudoers.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-# Users should be added to admin group in packer template
 groupadd -r admin -f
+usermod -a -G admin vagrant
 
 cp /etc/sudoers /etc/sudoers.orig
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
